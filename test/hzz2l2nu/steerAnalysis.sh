15df05e3083ce79d155c8963a379d00a1dd5744e
Pedro Silva <psilva@cern.ch>
2013-09-13 15:30:20 +0200

    Adding wrapper

---
+++ test/hzz2l2nu/steerAnalysis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# full analysis
+#
+
+step=$1
+optim_step=$2
+outdir="/afs/cern.ch/user/p/psilva/work/hzz_5311/"
+indir="/store/cmst3/user/psilva/5311_ntuples"
+cfg="$CMSSW_BASE/src/UserCode/llvv_fwk/test/runAnalysis_cfg.py.templ"
+
+#prepare output directories
+mkdir -p ${outdir}/ll/
+mkdir -p ${outdir}/g/raw
+mkdir -p ${outdir}/g/qt
+
+if [ "$step" == "1" ]; then
+    echo "Submitting first pass (warning no systs yet)"
+    runLocalAnalysisOverSamples.py -e runHZZ2l2nuAnalysis -j data/htozz_samples.json        -d ${indir} -o ${outdir}       -c ${cfg} -p "@runSystematics=False @useMVA=False @weightsFile='${CMSSW_BASE}/src/UserCode/llvv_fwk/data/weights/'" -s 8nh 
+    runLocalAnalysisOverSamples.py -e runHZZ2l2nuAnalysis -j data/htozz_photon_samples.json -d ${indir} -o ${outdir}/g/raw -c ${cfg} -p "@runSystematics=False @useMVA=True"                                                                   -s 1nd
+fi
+
+if [ "$step" == "2" ]; then
+    echo "Computing weights"
+    runPlotter --iLumi 19736 --inDir ${outdir}        --json data/htozz_samples.json        --outFile ${outdir}/plotter.root       --forceMerged #--noPlot
+    runPlotter --iLumi 19736 --inDir ${outdir}/g/raw/ --json data/htozz_photon_samples.json --outFile ${outdir}/plotter_g_raw.root --forceMerged #--noPlot
+    root -b -q "${CMSSW_BASE}/src/UserCode/llvv_fwk/test/ewkvp2j/FitQtSpectrum.C+(\"${outdir}/plotter.root\",\"${outdir}/plotter_g_raw.root\",ALL,HZZ)" 
+    mv gammawgts.root ${outdir}/gamma_weights.root
+fi
+
+if [ "$step" == "3" ]; then
+    echo "Submitting second pass, weighting in q_T"
+    runLocalAnalysisOverSamples.py -e runHZZ2l2nuAnalysis -j data/htozz_photon_samples.json -d ${indir} -o ${outdir}/g/qt -c ${cfg} -p "@runSystematics=False @useMVA=True @weightsFile='${outdir}/gamma_weights.root'" -s 8nh
+fi
+
+if [ "$step" == "4" ]; then
+    echo "FIXME"
+    echo "Running final plotter"
+    #runPlotter --iLumi 19736 --inDir ${outdir}/g/data/qt_tight/ --json data/vbfz_photon_samples.json --outFile ${outdir}/plotter_g_tight_qt.root --forceMerged --noPlot
+    #runPlotter --iLumi 19736 --inDir ${outdir}/g/data/qt_loose/ --json data/vbfz_photon_samples.json --outFile ${outdir}/plotter_g_loose_qt.root --forceMerged --noPlot
+fi
+
+if [ "$step" == "5" ]; then
+    echo "FIXME"
+    echo "Running optimization"
+    #python test/ewkvp2j/optimize_VBFZ.py -p ${optim_step} -b True -s Fisher_shapes -o Fisher_analysis
+fi
+
+
+
