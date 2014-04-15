e8e328bfe549f1357463434acfa1c89cca4a9924
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-04-15 18:13:30 +0200

    Added skeleton analysis for TStauStau. Inspired from the ZHTauTau analysis

---
+++ bin/runTStauStauAnalysisFWLite.cc
@@ -0,0 +1,216 @@
+#include "DataFormats/FWLite/interface/Handle.h"
+#include "DataFormats/FWLite/interface/Event.h"
+#include "DataFormats/FWLite/interface/ChainEvent.h"
+#include "DataFormats/Common/interface/MergeableCounter.h"
+
+#include "CondFormats/JetMETObjects/interface/JetResolution.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+
+#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/PythonParameterSet/interface/MakeParameterSets.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "PhysicsTools/Utilities/interface/LumiReWeighting.h"
+
+#include "TauAnalysis/CandidateTools/interface/NSVfitStandaloneAlgorithm.h" //for svfit
+
+
+#include "UserCode/llvv_fwk/interface/MacroUtils.h"
+#include "UserCode/llvv_fwk/interface/SmartSelectionMonitor.h"
+//#include "UserCode/llvv_fwk/interface/DataEventSummaryHandler.h"
+#include "UserCode/llvv_fwk/interface/llvvObjects.h"
+#include "UserCode/llvv_fwk/interface/TMVAUtils.h"
+#include "UserCode/llvv_fwk/interface/LeptonEfficiencySF.h"
+#include "UserCode/llvv_fwk/interface/PDFInfo.h"
+#include "UserCode/llvv_fwk/interface/MuScleFitCorrector.h"
+#include "UserCode/llvv_fwk/interface/GammaWeightsHandler.h"
+
+
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TEventList.h"
+#include "TROOT.h"
+#include "TNtuple.h"
+
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include <Math/VectorUtil.h>
+
+/*****************************************************************************/
+/* Return Codes:                                                             */
+/*   0 - Everything OK                                                       */
+/*   1 - Missing parameters_cfg.py configuration file                        */
+/*****************************************************************************/
+
+int main(int argc, char* argv[])
+{
+  /***************************************************************************/
+  /*                          Global Initialization                          */
+  /***************************************************************************/
+  // Check arguments:
+  if(argc<2)
+    std::cout << "Usage: " << argv[0] << " parameters_cfg.py" << std::endl, exit(1);
+
+  // Load FWLite:
+  gSystem->Load("libFWCoreFWLite");
+  AutoLibraryLoader::enable();
+
+  // Configure the process (aka "Load parameters from configuration file")
+  const edm::ParameterSet &runProcess = edm::readPSetsFrom(argv[1])->getParameter<edm::ParameterSet>("runProcess");
+  bool isMC = runProcess.getParameter<bool>("isMC");
+  double xsec = runProcess.getParameter<double>("xsec");
+  int mctruthmode = runProcess.getParameter<int>("mctruthmode"); // What is this one for?
+  //std::vector<int> jacknifeCfg = runProcess.getParameter<std::vector<int> >("jacknife");
+    //int jacknife(jacknifeCfg[0]), jacks(jacknifeCfg[1]);
+    //if(jacknife > 0 && jacks > 0) std::cout << "Jacknife will be applied to every " << jacknife << " out of " << jacks << " events." << std::endl;
+  std::vector<std::string> urls = runProcess.getParameter<std::vector<std::string> >("input");
+  std::string baseDir = runProcess.getParameter<std::string>("dirName");
+  std::string outdir = runProcess.getParameter<std::string>("outdir");
+  std::string jecDir = runProcess.getParameter<std::string>("jecDir");
+  bool runSystematics = runProcess.getParameter<bool>("runSystematics");
+
+  // Hardcoded configs
+  bool runLoosePhotonSelection(false); // Specific to ZHTauTau?
+  bool Cut_tautau_MVA_iso(true); // Specific to ZHTauTau?
+  float minJetPtToApply(30); // Min Jet Pt to accept jet?
+  bool examineThisEvent(false); // ?
+
+  // Lepton Efficiencies
+  LeptonEfficiencySF lepEff;
+  
+  
+  // Setting Up
+  gSystem->Exec(("mkdir -p " + outdir).c_str());
+  std::string url = urls[0];
+  std::string outFileUrl(gSystem->BaseName(url.c_str()));
+  while(outFileUrl.find(".root", 0) != std::string::npos)
+    outFileUrl.replace(outFileUrl.find(".root", 0), 5, "");
+  if(mctruthmode != 0)
+  {
+    outFileUrl += "_filt";
+    outFileUrl += mctruthmode;
+  }
+  std::string outUrl = outdir;
+
+
+
+  /***************************************************************************/
+  /*                         Initializing Histograms                         */
+  /***************************************************************************/
+  SmartSelectionMonitor mon;
+  TH1F *h = (TH1F*)mon.addHistogram(new TH1F("cutFlow", ";;Events", 20, 0, 20));
+  h->GetXaxis()->SetBinLabel(1, "All");
+  // ...
+  // TH2D* hist = (TH2D*)mon.addHistogram(...);
+
+
+
+  /***************************************************************************/
+  /*                          Prepare for Event Loop                         */
+  /***************************************************************************/
+  fwlite::ChainEvent ev(urls);
+  const Int_t totalEntries = ev.size();
+  
+  // MC normalization to 1/pb
+  double nInitEvent = 1.;
+  if(isMC)
+    nInitEvent = (double) utils::getMergeableCounterValue(urls, "startCounter");
+  double xsecWeight = xsec/nInitEvent;
+  if(!isMC)
+    xsecWeight = 1.;
+
+  // Jet Energy Scale and Uncertainties
+  gSystem->ExpandPathName(jecDir.c_str());
+  FactorizedJetCorrector *jesCor = utils::cmssw::getJetCorrector(jecDir, isMC);
+  JetCorrectionUncertainty *totalJESUnc = new JetCorrectionUncertainty((jecDir+"/MC_Uncertainty_AK5PFchs.txt"));
+  
+  // Muon Energy Scale and Uncertainties
+  MuScleFitCorrector* muCor = getMuonCorrector(jecDir, url);
+  
+  // Pileup Weighting: Based on vtx (For now?)
+  edm::LumiReWeighting* LumiWeights = NULL;
+  utils::cmssw::PuShifter_t PuShifters;
+  double PUNorm[] = {1, 1, 1};
+  if(isMC)
+  {
+    std::vector<double> dataPileupDistributionDouble = runProcess.getParameter<std::vector<double> >("datapileup");
+    std::vector<float> dataPileupDistribution;
+      for(unsigned int i = 0; i < dataPileupDistributionDouble.size(); ++i)
+        dataPileupDistribution.push_back(dataPileupDistributionDouble[i]);
+    std::vector<float> mcPileupDistribution;
+    
+    utils::getMCPileupDistribution(ev, dataPileupDistribution.size(), mcPileupDistribution);
+    while(mcPileupDistribution.size() < dataPileupDistribution.size())  mcPileupDistribution.push_back(0.0);
+    while(mcPileupDistribution.size() > dataPileupDistribution.size())  dataPileupDistribution.push_back(0.0);
+
+    LumiWeights= new edm::LumiReWeighting(mcPileupDistribution, dataPileupDistribution);
+    PuShifters=utils::cmssw::getPUshifters(dataPileupDistribution, 0.05);
+    utils::getPileupNormalization(mcPileupDistribution, PUNorm, LumiWeights, PuShifters);
+  }
+
+  gROOT->cd();  //THIS LINE IS NEEDED TO MAKE SURE THAT HISTOGRAM INTERNALLY PRODUCED IN LumiReWeighting ARE NOT DESTROYED WHEN CLOSING THE FILE
+
+
+
+  /***************************************************************************/
+  /*                               Event Loop                                */
+  /***************************************************************************/
+  std::cout << "       Progress Bar:0%      20%       40%       60%       80%      100%" << std::endl;
+  std::cout << "Scanning the ntuple:";
+
+  DuplicatesChecker duplicatesChecker;
+  int nDuplicates(0);
+  int step(totalEntries/50);
+
+  // Loop on events
+  for(int iev = 0; iev < totalEntries; ++iev)
+  {
+    if(iev%step == 0)
+      std::cout << "_" << std::flush;
+
+    double weight = 1;
+
+    // Prepare tags to fill the histograms
+    std::vector<TString> chTags;
+    chTags.push_back("all");
+
+    // First fill the all events bin in the cutflow:
+    mon.fillHisto("cutflow", chTags, 0, weight);
+
+    // Load the event content from tree
+//    ev.to(iev);
+  }
+
+  std::cout << std::endl;
+
+
+
+  /***************************************************************************/
+  /*                        Saving Histograms to File                        */
+  /***************************************************************************/
+  outUrl += "/";
+  outUrl += outFileUrl + ".root";
+  std::cout << "Saving results in " << outUrl << std::endl;
+  TFile* outfile = new TFile(outUrl.c_str(), "RECREATE");
+  mon.Write();
+  outfile->Close();
+  delete outfile;
+
+  outUrl.replace(outUrl.find(".root", 0), 5, "_summary.root");
+  std::cout << "Saving summary results in " << outUrl << std::endl;
+  outfile = new TFile(outUrl.c_str(), "RECREATE");
+  // Write Tuple/Tree
+  //summaryTuple->SetDirectory(ofile);
+  //summaryTuple->Write();
+  outfile->Close();
+  delete outfile;
+
+  return 0;
+}  
