f871f87bae3f4babe04b1c366c9b1cd279329bc9
Pietro Vischia <pietro.vischia@gmail.com>
2014-05-14 17:39:52 +0200

    Updated tau discriminator against muons tag

--- bin/runChHiggsAnalysisFWLite.cc
+++ bin/runChHiggsAnalysisFWLite.cc
@@ -1328,7 +1328,7 @@ int main(int argc, char* argv[])
 	  
 	  
 	  if(!tau.passId(llvvTAUID::againstElectronMediumMVA5))continue;
-	  if(!tau.passId(llvvTAUID::againstMuonTight2))continue; 
+	  if(!tau.passId(llvvTAUID::againstMuonTight3))continue; 
 	  if(!tau.passId(llvvTAUID::decayModeFinding))continue;
 	  if(!tau.passId(llvvTAUID::byMediumCombinedIsolationDeltaBetaCorr3Hits))continue;
 	  
