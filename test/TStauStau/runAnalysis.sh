261960e95fac8714c1ffc2476eaf533cc20b21c3
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-06-08 17:17:02 +0200

    Added summary tree saving option to TStauStau analysis

--- test/TStauStau/runAnalysis.sh
+++ test/TStauStau/runAnalysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Inclusive
-runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /store/group/phys_higgs/cmshzz2l2v/2014_04_20/ -o Processed_Inclusive -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=False @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=False" -s 8nh
+runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /store/group/phys_higgs/cmshzz2l2v/2014_04_20/ -o Processed_Inclusive -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=False" -s 1nd
 # Exclusive
 #runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /store/group/phys_higgs/cmshzz2l2v/2014_04_20/ -o Processed_Exclusive -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=False @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=True" -s 8nh
