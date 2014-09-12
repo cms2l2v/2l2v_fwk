5cac352770db891a66998c097d0fb34502dc87c7
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-09-12 15:57:21 +0100

    Added plotting of 2012BC

--- test/TStauStau/makePlots.sh
+++ test/TStauStau/makePlots.sh
@@ -15,6 +15,13 @@ runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/
 ## IPM
 runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012C/ --outFile plotter_2012C_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012C.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 
+# Only Run2012BC
+# Exclusive
+## LIP
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+## IPM
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+
 # Only Run2012D
 # Inclusive
 #runPlotterFWLite --iEcm 8 --iLumi 7317 --inDir Processed_Inclusive/ --outDir ~/www/TStauStau/MCvsData/2012D/inclusive/ --outFile plotter_2012D_Inclusive.root --json tstaustau_samples_inclusive_2012D.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
