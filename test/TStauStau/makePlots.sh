efac12c824535877c030efc3c3560c7804d314c2
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-09-15 15:46:30 +0100

    Updated makePlots.sh

--- test/TStauStau/makePlots.sh
+++ test/TStauStau/makePlots.sh
@@ -4,21 +4,21 @@
 
 # Only Run2012B
 ## LIP
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 4429 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012B/ --outFile plotter_2012B_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012B.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 4429 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012B/ --outFile plotter_2012B_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012B.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 ## IPM
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 4429 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012B/ --outFile plotter_2012B_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012B.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 4429 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012B/ --outFile plotter_2012B_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012B.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 
 # Only Run2012C
 ## LIP
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012C/ --outFile plotter_2012C_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012C.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012C/ --outFile plotter_2012C_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012C.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 ## IPM
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012C/ --outFile plotter_2012C_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012C.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 7147 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012C/ --outFile plotter_2012C_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012C.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 
 # Only Run2012BC
 ## LIP
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ --outDir ./LIP_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_LIP.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 ## IPM
-#runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 11576 --inDir /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ --outDir ./IPM_Selection_2012BC/ --outFile plotter_2012BC_Exclusive_IPM.root --json tstaustau_samples_exclusive_2012BC.json --plotExt .png --plotExt .pdf --plotExt .C --plotExt .root
 
 # Only Run2012D
 ## LIP
