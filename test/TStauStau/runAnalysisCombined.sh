323d6f28d81a68251a27d03f52b5f5bbc5fff304
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-09-19 18:33:22 +0100

    Fixed submission script for combined selection and added script to make plots for combined selection

--- test/TStauStau/runAnalysisCombined.sh
+++ test/TStauStau/runAnalysisCombined.sh
@@ -2,4 +2,4 @@
 
 # Exclusive without quick fix
 ## IPM selection with modifications
-runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection_LIPTauID/ -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=True @selection=IPM @doLIPTauID=True @doLIPBVeto=False @periodHLT=2012B @doCombined=True" -s 8nh
+runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection_Modified/ -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=True @selection=IPM @doLIPTauID=True @doLIPBVeto=False @periodHLT=2012B @doCombined=True" -s 8nh
