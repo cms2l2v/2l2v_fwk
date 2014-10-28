f4aca87c4eb7b565d5fe0bfd4086b8d2733568bb
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-10-28 11:30:33 +0000

    Added script to run the analysis

---
+++ test/TStauStau/runAnalysis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/Results/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @doSVfit=False" -s 8nh
