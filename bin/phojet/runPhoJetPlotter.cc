6b05e19e049e20b245431be9311dd9d448e67f6d
Xin Shi <xshi@xshi.org>
2015-01-05 15:24:56 -0500

    start

---
+++ bin/phojet/runPhoJetPlotter.cc
@@ -0,0 +1,46 @@
+// -*- C++ -*-
+//
+// Xin Shi <Xin.Shi@cern.ch>
+// Mon Jan 5 13:10:33 EST 2015
+// 
+// Plotter for the photon + jet analysis 
+// 
+
+
+#include <iostream>
+#include <string>
+
+
+void print_usage() {
+  printf("--help    --> print this helping text\n");
+  printf("--inDir   --> path to the directory containing the .root files to process\n");
+  printf("--outDir  --> path of the directory that will contains the output plots and tables\n");
+  printf("--outFile --> path of the output summary .root file\n");
+  printf("--json    --> containing list of process (and associated style) to process to process\n");
+}
+
+int main(int argc, char* argv[])
+{
+  // check arguments
+  if(argc<2){
+    print_usage();
+    return 0;
+  }
+  
+  for(int i=1;i<argc;i++){
+    std::string arg(argv[i]);
+    if (arg.find("--help") != std::string::npos){
+      print_usage();
+      return 0; 
+    }
+  }
+    
+
+}  
+
+
+
+
+
+
+
