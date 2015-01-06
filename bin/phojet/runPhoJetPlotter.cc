4748e723c91afab5fdb065a0a06d9dcb2d9069d3
Xin Shi <xshi@xshi.org>
2015-01-06 16:44:10 -0500

    start the getlistofobject

--- bin/phojet/runPhoJetPlotter.cc
+++ bin/phojet/runPhoJetPlotter.cc
@@ -9,6 +9,24 @@
 
 #include <iostream>
 #include <string>
+#include <list>
+
+#include "TDirectory.h"
+
+#include "UserCode/llvv_fwk/interface/JSONWrapper.h"
+
+struct NameAndType{
+   std::string name;
+   int type; 
+   bool isIndexPlot;
+   NameAndType(std::string name_,  int type_, bool isIndexPlot_){name = name_; type = type_; isIndexPlot = isIndexPlot_;}
+   bool is1D()  {return type==1;}
+   bool is2D()  {return type==2;}
+   bool is3D()  {return type==3;}
+   bool isTree(){return type==4;}
+   bool operator==(const NameAndType& a){ return a.name == name;}
+   bool operator< (const NameAndType& a){ return a.name < name;}
+ };
 
 
 void print_usage() {
@@ -17,6 +35,19 @@ void print_usage() {
   printf("--outDir  --> path of the directory that will contains the output plots and tables\n");
   printf("--outFile --> path of the output summary .root file\n");
   printf("--json    --> containing list of process (and associated style) to process to process\n");
+}
+
+void GetListOfObject(JSONWrapper::Object& Root,
+		     std::string RootDir,
+		     std::list<NameAndType>& histlist,
+		     TDirectory* dir=NULL,
+		     std::string parentPath=""){
+
+  if(parentPath=="" && !dir){
+    std::vector<JSONWrapper::Object> Process = Root["proc"].daughters();
+    std::cout << "Num of Process: " << Process.size() << std::endl; 
+
+  } // only three arg case 
 }
 
 
@@ -59,7 +90,11 @@ int main(int argc, char* argv[])
   }
 
   system( (std::string("mkdir -p ") + outDir).c_str());
-  
+
+  JSONWrapper::Object Root(jsonFile, true);
+  std::list<NameAndType> histlist;
+  GetListOfObject(Root,inDir,histlist);
+    
 }  
 
 
