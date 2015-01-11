681b85b850aa8dbffc376490c3a8be307614dde7
Xin Shi <xshi@xshi.org>
2015-01-11 11:53:26 -0500

    add QCD sample

--- test/phojet/crabConfig.py
+++ test/phojet/crabConfig.py
@@ -3,7 +3,8 @@ config = Configuration()
 
 config.section_("General")
 #config.General.requestName = 'GJets_HT-100to200'
-config.General.requestName = 'GJet_Pt-15to3000'
+#config.General.requestName = 'GJet_Pt-15to3000'
+config.General.requestName = 'QCD_Pt-15to3000'
 config.General.workArea = 'crab_phojet'
 
 config.section_("JobType")
@@ -16,7 +17,8 @@ config.JobType.outputFiles = ['output.root']
 
 config.section_("Data")
 #config.Data.inputDataset = '/GJets_HT-100to200_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-config.Data.inputDataset = "/GJet_Pt-15to3000_Tune4C_13TeV_pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM"
+#config.Data.inputDataset = "/GJet_Pt-15to3000_Tune4C_13TeV_pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM"
+config.Data.inputDataset = "/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM"
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
