ffbe477a18b45d7ce8297bcb2be05c63671a12d6
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-01-22 18:14:52 +0000

    Removing old analysis

--- test/TStauStau/old_analysis/runAnalysisLIPvsIPM.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exclusive without quick fix
-## LIP
-runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/LIP_selection/ -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=True @selection=LIP @doLIPTauID=True @doLIPBVeto=True @periodHLT=2012B @doCombined=False @doTauAgainstElectronLoose=False @noTauOverlap=False @doSVfit=False" -s 8nh
-## IPM
-runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/IPM_selection/ -c runAnalysis_cfg.py.templ -p "@useMVA=True @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0 @exclusiveRun=True @selection=IPM @doLIPTauID=False @doLIPBVeto=False @periodHLT=2012B @doCombined=False @doTauAgainstElectronLoose=False @noTauOverlap=False @doSVfit=True" -s 8nh
