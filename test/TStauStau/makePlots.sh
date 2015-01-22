c3863b301d79603e0ef25166ea1e6e00d7adeb41
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-01-22 15:27:44 +0000

    Updated plotter

--- test/TStauStau/makePlots.sh
+++ test/TStauStau/makePlots.sh
@@ -2,19 +2,19 @@
 
 #rm ~/www/TStauStau/MCvsData/2012{B,C,D,BCD}/{exclusive,inclusive}{/,/DY/}*.{png,pdf,root,C,tex}
 OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/Plots
-INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/Results
+INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewResults
 
 ## 2012ABCD
 runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
 
 ## 2012A
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 876.225 --inDir $INDIR/ --outDir "$OUTDIR""_2012A/" --outFile "$OUTDIR""_2012A/plotter.root" --json tstaustau_samples_2012A.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 876.225 --inDir $INDIR/ --outDir "$OUTDIR""_2012A/" --outFile "$OUTDIR""_2012A/plotter.root" --json tstaustau_samples_2012A.json --plotExt .png --plotExt .root
 
 ## 2012B
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 4412 --inDir $INDIR/ --outDir "$OUTDIR""_2012B/" --outFile "$OUTDIR""_2012B/plotter.root" --json tstaustau_samples_2012B.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 4412 --inDir $INDIR/ --outDir "$OUTDIR""_2012B/" --outFile "$OUTDIR""_2012B/plotter.root" --json tstaustau_samples_2012B.json --plotExt .png --plotExt .root
 
 ## 2012C
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 7055 --inDir $INDIR/ --outDir "$OUTDIR""_2012C/" --outFile "$OUTDIR""_2012C/plotter.root" --json tstaustau_samples_2012C.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 7055 --inDir $INDIR/ --outDir "$OUTDIR""_2012C/" --outFile "$OUTDIR""_2012C/plotter.root" --json tstaustau_samples_2012C.json --plotExt .png --plotExt .root
 
 ## 2012D
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 7329 --inDir $INDIR/ --outDir "$OUTDIR""_2012D/" --outFile "$OUTDIR""_2012D/plotter.root" --json tstaustau_samples_2012D.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 7329 --inDir $INDIR/ --outDir "$OUTDIR""_2012D/" --outFile "$OUTDIR""_2012D/plotter.root" --json tstaustau_samples_2012D.json --plotExt .png --plotExt .root
