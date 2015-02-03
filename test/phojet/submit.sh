179a7484a1a3c63911d2c967d4118709a1bb01f7
Xin Shi <xshi@xshi.org>
2015-02-03 08:36:04 -0500

    comment out x509

--- test/phojet/submit.sh
+++ test/phojet/submit.sh
@@ -13,9 +13,9 @@ ls -lR
 cp ./lib/$SCRAM_ARCH/* $CMSSW_BASE/lib/$SCRAM_ARCH
 cp runPhoJetAnalysis $CMSSW_BASE/bin/$SCRAM_ARCH
 
-# Setup grid proxy
-cp x509_proxy $CMSSW_BASE/
-export X509_USER_PROXY=$CMSSW_BASE/x509_proxy
+# # Setup grid proxy # no need after crab 3.3.12 
+# cp x509_proxy $CMSSW_BASE/
+# export X509_USER_PROXY=$CMSSW_BASE/x509_proxy
 
 # Now run the analysis
 runPhoJetAnalysis phojet_cfg.py
