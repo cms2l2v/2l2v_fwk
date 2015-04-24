ba9864b014c6dd1633de7ed306ed4d1eac3898f3
Xin Shi <xshi@xshi.org>
2015-04-24 10:24:19 -0500

    pho2

--- test/phojet/drawPt.cc
+++ test/phojet/drawPt.cc
@@ -11,6 +11,7 @@
 #include <TH1F.h>
 #include <TStyle.h> 
 #include <TROOT.h> 
+#include <TLegend.h> 
 
 void set_root_style(int stat=1110, int grid=0){
   gROOT->Reset();
@@ -52,26 +53,56 @@ void set_root_style(int stat=1110, int grid=0){
 
 
 TCanvas* drawPt(TString label, std::vector<TString> inputFiles){
-  printf("label = %s \n", label.Data());
-
+  if (label == "pho2") printf("label = %s \n", label.Data());
   int ww(800), wh(800);
   set_root_style();
   TCanvas *c = new TCanvas("c", "Transverse momentum", ww, wh);
+
+  TH1F *h = NULL;
+  TH1F *h2 = NULL; 
+
+  TLegend *leg = new TLegend(0.2, 0.6, 0.5, 0.8);
+  leg->SetBorderSize(0);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetNColumns(1);
+  leg->SetTextSize(0.02);
+  leg->SetTextSizePixels(25);
+
 
   for (std::vector<int>:: size_type i = 0; i != inputFiles.size(); i++) {    
     TFile *f = new TFile(inputFiles[i]);
     TString dir = "#gamma+jets_pT-15to3000"; 
     TString histName = Form("%s/qt", dir.Data()); 
-    TH1F *h = (TH1F*)f->Get(histName);
+    h = (TH1F*)f->Get(histName);
     if (!h) {
       std::cout << "Not able to find hist: " << histName << std::endl; 
       return NULL; 
     }
+    
+    if (label == "pho2") {
+      TString histName2 = Form("%s/trg_phopt", dir.Data()); 
+      h2 = (TH1F*)f->Get(histName2);
+      if (!h2) {
+	std::cout << "Not able to find hist: " << histName2 << std::endl; 
+	return NULL; 
+      }
+
+      leg->AddEntry(h2, "triggerd");
+      h2->SetMarkerColor(2);
+      h2->SetTitle(""); 
+      h2->Draw();
+    }
 
     h->GetXaxis()->SetRangeUser(10,1000);
-    h->Draw();
-    
+    if (label == "pho2") {
+      leg->AddEntry(h, "selected"); 
+      h->Draw("same");
+      leg->Draw(); 
+    }
+    else {h->Draw();}
   }
+
   c->SetLogx();
   c->SetLogy();
   c->Update(); 
