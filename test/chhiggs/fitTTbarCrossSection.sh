5bd7f3236386d3b9e4c7d19d3411662ad3340e39
Pietro Vischia <pietro.vischia@gmail.com>
2015-06-04 13:06:40 +0200

    Rationalize production of datacards for fits

--- test/chhiggs/fitTTbarCrossSection.sh
+++ test/chhiggs/fitTTbarCrossSection.sh
@@ -8,25 +8,35 @@ JSONFILELEPTAU=$CMSSW_BASE/src/UserCode/llvv_fwk/data/chhiggs/phys14_xsec_lepton
 # histo choices for dilepton: finalnbjets, finalmt, eventflow(bin6)
 # histo choices for leptau: finaltaur, finaltaupolarization, finaldphilepmet, finaldphitaumet, finaldphileptau, finaltaupt, eventflowslep(bin6)
 # Remember to blank the BINS* variables when not using eventflow histograms ;)
-HISTODILEPTON=finalmt
-HISTOLEPTAU=finaltaupolarization
-#HISTODILEPTON=eventflow
-#HISTOLEPTAU=eventflowslep
-BINSDILEPTON=""
-BINSLEPTAU=""
-#BINSDILEPTON=" --bins 6 "
-#BINSLEPTAU=" --bins 6 "
-
-OUTDILEPTON=~/www/13TeV_xsec/dileptons/
-OUTLEPTAU=~/www/13TeV_xsec/leptau/
 
 
+for HISTODILEPTON in finalnbjets finalmt eventflow
+do
+    OUTDILEPTON=~/www/13TeV_xsec/dileptons/${HISTODILEPTON}/
+    BINSDILEPTON=""
+    BINSLEPTAU=""
+    if [ "${HISTODILEPTON}" == "eventflow" ]
+    then
+        BINSDILEPTON=" --bins 6 "
+        BINSLEPTAU=" --bins 6 "
+    fi
+    # Dileptons
+    fitTTbarCrossSection --out ${OUTDILEPTON} --in ${DIR}plotter_dilepton.root --json ${JSONFILEDILEPTON} --histo ${HISTODILEPTON} ${BINSDILEPTON} --ch emu,ee,mumu
+    cp ~/www/HIG-13-026/index.php ${OUTDILEPTON}
 
-# Dileptons
-fitTTbarCrossSection --out ${OUTDILEPTON} --in ${DIR}plotter_dilepton.root --json ${JSONFILEDILEPTON} --histo ${HISTODILEPTON} ${BINSDILEPTON} --ch emu,ee,mumu
+done
 
-# Leptontau
-fitTTbarCrossSection --out ${OUTLEPTAU}   --in ${DIR}plotter_ltau.root     --json ${JSONFILELEPTAU}   --histo ${HISTOLEPTAU} ${BINSLEPTAU} --ch singlemu,singlee
-
-cp ~/www/HIG-13-026/index.php ${OUTDILEPTON}
-cp ~/www/HIG-13-026/index.php ${OUTLEPTAU}
+for HISTOLEPTAU in finaltaur finaltaupolarization finaldphilepmet finaldphitaumet finaldphileptau finaltaupt eventflowslep
+do
+    OUTLEPTAU=~/www/13TeV_xsec/leptau/${HISTOLEPTAU}/
+    BINSDILEPTON=""
+    BINSLEPTAU=""
+    if [ "${HISTOLEPTAU}" == "eventflowslep" ]
+    then
+        BINSDILEPTON=" --bins 6 "
+        BINSLEPTAU=" --bins 6 "
+    fi
+    # Leptontau
+    fitTTbarCrossSection --out ${OUTLEPTAU}   --in ${DIR}plotter_ltau.root     --json ${JSONFILELEPTAU}   --histo ${HISTOLEPTAU} ${BINSLEPTAU} --ch singlemu,singlee
+    cp ~/www/HIG-13-026/index.php ${OUTLEPTAU}
+done
