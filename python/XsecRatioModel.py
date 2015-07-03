f641c2875852b69a566c5875e4d1b149e8cf57f9
Pietro Vischia <pietro.vischia@gmail.com>
2015-07-03 18:31:21 +0200

    Physics model for the ratio lep-tau/emu

--- python/XsecRatioModel.py
+++ python/XsecRatioModel.py
@@ -6,16 +6,21 @@ class XsecRatioModel(PhysicsModel):
 
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
-        self.modelBuilder.doVar('ratio[1,0,2]');
-        self.modelBuilder.doVar('commonxsec[1,0,2]');
-        self.modelBuilder.doSet('POI','ratio')
+        self.modelBuilder.doVar('brratio[1,0,10]');
+        self.modelBuilder.doVar('commonxsec[1,0,10]')
+        self.modelBuilder.out.var("brratio").setRange(0,10)
+        self.modelBuilder.out.var("brratio").setConstant(False)
+        self.modelBuilder.out.var("commonxsec").setRange(0,10)
+        self.modelBuilder.out.var("commonxsec").setConstant(False)
+        self.modelBuilder.doSet("POI",'brratio')
+
         # mutau = commonxsec*ratio
         # emu = commonxsec
         # mutau/emu = commonxsec*ratio/commonxsec = ratio
-        self.modelBuilder.factory_('expr::Scaling_ltau("@0*@1", commonxsec, ratio)')
+        self.modelBuilder.factory_('expr::Scaling_ltau("@0*@1", commonxsec, brratio)')
         self.modelBuilder.factory_('expr::Scaling_emu("@0", commonxsec)')
 
-        self.processScaling = { 'emu_signal':'emu', 'mutau_signal':'ltau', 'etau_signal':'ltau'}
+        self.processScaling = { 'signal_emu':'emu', 'signal_singlemu':'ltau', 'signal_singlee':'ltau'}
 
         self.modelBuilder.out.Print()
         
@@ -23,6 +28,7 @@ class XsecRatioModel(PhysicsModel):
 
         for prefix, model in self.processScaling.iteritems():
             if process.startswith(prefix):
+                print "Process ",process,"will be scaled according to the model"
                 return 'Scaling_'+model
             
         return 1
--- test/chhiggs/fitRatio.sh
+++ test/chhiggs/fitRatio.sh
@@ -9,10 +9,17 @@ RATIOAREA="${OUTRATIO}fit"
 
 mkdir -p ${RATIOAREA}
 
+echo "COMBINING CARDS"
 combineCards.py Name1=${DEMU} Name2=${DMUTAU} > ${DRATIO}
 
-text2workspace.py -P UserCode.llvv_fwk.XsecRatioModel:xsecRatioModel ${DRATIO} -o ${WRATIO}
+echo "BUILDING WORKSPACE"
+text2workspace.py -v 3 -P UserCode.llvv_fwk.XsecRatioModel:xsecRatioModel ${DRATIO} -o ${WRATIO}
 
+dot -Tpng -o ${WRATIO}_directedgraph.png ${WRATIO}.dot
+fdp -Tpng -o ${WRATIO}_springmodel.png ${WRATIO}.dot
+
+convert ${WRATIO}_directedgraph.png ${WRATIO}_directedgraph.pdf
+convert ${WRATIO}_springmodel.png   ${WRATIO}_springmodel.pdf
 # runPLRanalysis --in ${WRATIO} --lumi 1000 --ecm 13 --cl 0.68 
 # mv PLR* ${OUTRATIO}
 # mv textable.tex ${OUTRATIO}
