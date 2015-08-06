c4f87f1d984ae5b32718a4a35af10ab9bdbb6fde
Pietro Vischia <pietro.vischia@gmail.com>
2015-08-06 17:19:56 +0100

    Added stalking option to countBatchJobs (works only at LIP)

--- test/chhiggs/countBatchJobs.sh
+++ test/chhiggs/countBatchJobs.sh
@@ -2,6 +2,15 @@
 
 #for i in {1..10000}
 
+USER=""
+if [ $# -eq 0 ]
+  then
+    USER=`whoami`
+  else
+    USER=${1}
+fi
+
+
 LIP=true
 if [ `which qstat ` ]
 then
@@ -12,7 +21,13 @@ while true ;
 	do
          if [ ${LIP} ]
 	 then
-	     echo "running: " `qstat -u vischia | grep vischia | grep " r " | wc -l` "    total: " `qstat -u vischia | grep vischia | wc -l` 
+	     if [ "${USER}" = "stalker" ]
+	     then
+		 echo "You are running in stalker mode, jajajajajajajajajaja"
+		 echo "Vischia( run " `qstat -u vischia | grep " r " | wc -l` ", tot: " `qstat -u vischia | grep ingrid.pt | wc -l`  "). Cris( run " `qstat -u cbeiraod | grep " r " | wc -l` ", tot: " `qstat -u cbeiraod | grep ingrid.pt | wc -l` "). Bargassa( run " `qstat -u bargassa | grep " r " | wc -l` ", tot: " `qstat -u bargassa | grep ingrid.pt | wc -l` ")"
+	     else
+		 echo "running: " `qstat -u ${USER} | grep " r " | wc -l` "    total: " `qstat -u ${USER} | wc -l` 
+	     fi
 	 else
 	     echo "RUNNING: `bjobs | grep RUN | wc -l`            PENDING: `bjobs | grep PEND | wc -l`"
 	 fi
