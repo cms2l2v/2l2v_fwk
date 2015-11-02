dac769be7807a52b064f2fb2c3c0886888e5ce87
Pietro Vischia <pietro.vischia@gmail.com>
2015-11-02 16:31:51 +0000

    Ntuplizer embedded in submission tool

--- test/chhiggs/doAnal.sh
+++ test/chhiggs/doAnal.sh
@@ -34,10 +34,10 @@ if [ "${1}" = "submit" ]; then
     fi
     
     if [ "${2}" = "ntuplizer" ]; then
-        OUTDIR=$CMSSW_BASE/src/UserCode/llvv_fwk/test/chhiggs/ntuples_ttbar/
+        OUTDIR=$CMSSW_BASE/src/UserCode/llvv_fwk/test/chhiggs/ntuples_4_ttbar/
         mkdir -p ${OUTDIR}
         QUEUE="crab"
-        LFNDIRBASE="ntuples_pietro_2015-10-26/"
+        LFNDIRBASE="ntuples_pietro_2015-10-29_v1bis/"
         runAnalysisOverSamples.py -e runNtuplizer -j ${JSONFILE} -o ${OUTDIR} -d  /dummy/ -c $CMSSW_BASE/src/UserCode/llvv_fwk/test/runAnalysis_cfg.py.templ -p "@useMVA=False @saveSummaryTree=True @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0" ${RESUBMIT} -l ${LFNDIRBASE} -f 8 -s ${QUEUE}
     else
         runAnalysisOverSamples.py -e runChHiggsAnalysis -j ${JSONFILE} -o ${OUTDIR} -d  /dummy/ -c $CMSSW_BASE/src/UserCode/llvv_fwk/test/runAnalysis_cfg.py.templ -p "@useMVA=False @saveSummaryTree=False @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0" ${RESUBMIT} -f 8 -s ${QUEUE}
