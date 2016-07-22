b42103c17e3b022e279fe7e985dec25ab97dac53
alessio <alessio.magitteri@cern.ch>
2016-07-22 20:29:05 +0200

    th1morph.h fixed

--- bin/common/runPlotter.cc
+++ bin/common/runPlotter.cc
@@ -35,7 +35,7 @@
 #include "UserCode/llvv_fwk/interface/MacroUtils.h"
 #include "UserCode/llvv_fwk/interface/RootUtils.h"
 #include "UserCode/llvv_fwk/interface/JSONWrapper.h"
-#include "UserCode/llvv_fwk/interface/th1fmorph.h"
+#include "HiggsAnalysis/CombinedLimit/interface/th1fmorph.h"
 
 using namespace std;
 
