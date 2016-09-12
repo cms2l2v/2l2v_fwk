11b9fb52fff0fb73f79a0686757e0cdce8fd3f80
Georgia <georgia.karapostoli@gmail.com>
2016-09-12 18:32:22 +0200

    small updates and cleaning up

--- test/hzz2l2v/submit.sh
+++ test/hzz2l2v/submit.sh
@@ -62,13 +62,13 @@ if [[ $step == 0 ]]; then
 	if [[ $answer == "y" ]];
 	then
 	    echo "CLEANING UP..."
-	    rm -rdf $RESULTSDIR $PLOTSDIR LSFJOB_* core.* *.sh.e* *.sh.o* big-submission-*
+	    rm -rdf $RESULTSDIR $PLOTSDIR LSFJOB_* core.* *.sh.e* *.sh.o*
 	fi
 fi #end of step0
 
 ###  ############################################## STEPS between 1 and 2
 if [[ $step > 0.999 &&  $step < 2 ]]; then
-  if [[ $step == 1 || $step == 1.0 ]]; then        #submit jobs for 2l2v analysis
+   if [[ $step == 1 || $step == 1.0 ]]; then        #submit jobs for 2l2v analysis
 	echo "JOB SUBMISSION"
 	runAnalysisOverSamples.py -e runHZZ2l2vAnalysis -j $JSON -o $RESULTSDIR  -c $MAINDIR/../runAnalysis_cfg.py.templ -p "@data_pileup="$pileup" @jacknife=0 @saveSummaryTree=True @runSystematics=False @useMVA=True @jacks=0" -s $queue --report True --key 2l2v_mcbased $arguments
    fi
@@ -82,9 +82,6 @@ if [[ $step > 0.999 &&  $step < 2 ]]; then
 	echo "JOB SUBMISSION for Photon + Jet analysis with photon re-weighting"                                                                        
         runAnalysisOverSamples.py -e runHZZ2l2vAnalysis -j $JSON -o $RESULTSDIR -c $MAINDIR/../runAnalysis_cfg.py.templ -p "@data_pileup="$pileup" @useMVA=True @saveSummaryTree=True @weightsFile=$PWD/photonWeights_RunD.root @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0" -s $queue --report True --key 2l2v_photonsOnly $arguments 
    fi                              
-
-	 if [[ $HOSTNAME =~ "iihe" ]]; then yes | big-submission $RESULTSDIR/FARM/inputs/big.cmd; fi
- 
 fi
 
 ###  ############################################## STEPS between 2 and 3
@@ -111,7 +108,7 @@ if [[ $step > 1.999 && $step < 3 ]]; then
 	pip install --upgrade --install-option="--prefix=$HOME/.local" brilws &> /dev/null #will be installed only the first time
 
 	if [[ $JSON =~ "2016" ]]; then
-	  brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -i $RESULTSDIR/json_all.json -u /pb -o $RESULTSDIR/LUMI.txt 
+	    brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -i $RESULTSDIR/json_all.json -u /pb -o $RESULTSDIR/LUMI.txt 
 	else
 	    brilcalc lumi --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/moriond16_normtag.json -i $RESULTSDIR/json_all.json -u /pb -o $RESULTSDIR/LUMI.txt 
 	fi
