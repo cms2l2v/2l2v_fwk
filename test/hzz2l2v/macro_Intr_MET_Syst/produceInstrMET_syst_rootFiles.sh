cb63637207fddaff00a0548aa75c06f7142c755e
Hugo <hugo.delannoy@hotmail.com>
2017-05-26 14:44:52 +0200

    Systematics root file production for InstrMET

---
+++ test/hzz2l2v/macro_Intr_MET_Syst/produceInstrMET_syst_rootFiles.sh
@@ -0,0 +1,13 @@
+cp MakeSyst_custom_forMTandMET.C test.C 
+rm -f InstrMET_systematics.root
+#all except gamma stats
+sed 's/GAMMASTATS_ONLY true/GAMMASTATS_ONLY false/' test.C | sed 's/void MakeSyst_custom_forMTandMET()/void tmpMakeSyst()/' > tmpMakeSyst.C
+root -l -q -b tmpMakeSyst.C
+mv InstrMET_systematics.root $CMSSW_BASE/src/UserCode/llvv_fwk/data/InstrMET_systematics/InstrMET_systematics_ALL_EXCEPT_GAMMASTATS.root 
+rm -f tmpMakeSyst.C
+#Gamma stats only
+sed 's/ALL true/ALL false/' test.C | sed 's/WGAMMA_ONLY true/WGAMMA_ONLY false/' | sed 's/ZGAMMA_ONLY true/ZGAMMA_ONLY false/' | sed 's/WJETS_ONLY true/WJETS_ONLY false/' | sed 's/METHOD_ONLY true/METHOD_ONLY false/' | sed 's/void MakeSyst_custom_forMTandMET()/void tmpMakeSyst()/' > tmpMakeSyst.C
+root -l -q -b tmpMakeSyst.C
+mv InstrMET_systematics.root $CMSSW_BASE/src/UserCode/llvv_fwk/data/InstrMET_systematics/InstrMET_systematics_GAMMASTATS.root
+rm -f tmpMakeSyst.C
+rm -f test.C
